4026145403a0d51a184983843f1819caab3b6f19
Dave Sanders <davepaulsanders@gmail.com>
2022-05-01 19:47:51 -0400

    Added ability to create new notes and automatically add them to notes list

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const notes = require("../../db/db.json");
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
   if (notes) {
@@ -7,4 +9,13 @@ router.get("/notes", (req, res) => {
   }
 });
 
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify(notes, null, 2)
+  );
+  res.send(notes);
+});
 module.exports = router;
