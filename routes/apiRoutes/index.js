b615e577f6ab0b6d1892495196b71f85cd1a8c7d
Dave Sanders <davepaulsanders@gmail.com>
2022-05-02 11:18:52 -0400

    Added uniqid to create ids for each note

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const notes = require("../../db/db.json");
 const fs = require("fs");
 const path = require("path");
-
+const uniqid = require("uniqid");
 router.get("/notes", (req, res) => {
   if (notes) {
     res.json(notes);
@@ -11,6 +11,7 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
   const newNote = req.body;
+  newNote.id = uniqid().toString();
   notes.push(newNote);
   fs.writeFileSync(
     path.join(__dirname, "../../db/db.json"),
